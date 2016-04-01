a872911504fcc35b3400db370712878c70c7bc99
kbharathala <kbharathala@gmail.com>
2016-03-31 20:21:56 -0400

    Cleaned the Webpages and wrote to excel sheet for more easy access

--- wikiScraper.py
+++ wikiScraper.py
@@ -1,45 +1,52 @@
 import urllib2
 import re
+import csv
 
-f = urllib2.urlopen("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+BASE_URL = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+COMPANY_COUNT = 1008
+
+def file_cleaner(link):
+    f = urllib2.urlopen(link)
+    text = f.read()
+
+    # Removes all the html and extra white space
+    text = re.sub('<[^>]*>', '', text)
+    text = " ".join(text.split())
+    
+    # Removes everything after See Also
+    first = text.find("See also")
+    second = text[first+1:].find("See also")
+    text = text[:first+second]
+
+    # Removes everything before the start of the article
+    first = text.find("navigation, search")
+    text = text[first+18:]
+
+    # Removes everything between brackets.
+    text = re.sub(r'\[[^)]*\]', '', text)
+    return text
+
+f = urllib2.urlopen(BASE_URL)
 text = f.read()
-# text = re.sub('<[^>]*>', '', text)
-# text = " ".join(text.split())
-# first = text.find("See also")
-# second = text[first+1:].find("See also")
-# text = text[:first+second]
 
-
-
-#print(text)
-
-count = 0
-company_links = []
-company_text = {}
-while((text.find("<td><a href=\"/wiki/") != -1 or text.find("<td><a href=\"/w/index.php?title=") != -1) and count < 1008):
-	if(text.find("<td><a href=\"/wiki/") > text.find("<td><a href=\"/w/index.php?title=")):
-		text = text[text.find("(page does not exist)")+10:]
-		count += 1
-	else:
-		start = text.find("<td><a href=\"/wiki/")
-		text = text[start+1:]
-		end = text.find("title")
-		if(count %2 == 0):
-			company_name = text[18:end-2]
-			link = "https://en.wikipedia.org/wiki/" + company_name
-			company_text[company_name] = urllib2.urlopen(link).read()
-			company_links.append(link)
-		count += 1
-		text = text[end:]
-
-print(company_text)
-print('hello')
-
-# count = 0
-# for link in company_links:
-# 	count += 1
-# 	f = urllib2.urlopen(link)
-# 	company_text.append(f)
-# 	if(count % 10 == 0):
-# 		print(count)
-# print(f)
+with open('results.csv', 'wb') as f:
+    writer = csv.writer(f)
+    count = 0
+    company_links = []
+    #company_text = {}
+    while((text.find("<td><a href=\"/wiki/") != -1 or text.find("<td><a href=\"/w/index.php?title=") != -1) and count < COMPANY_COUNT*2 + 8):
+        if(text.find("<td><a href=\"/wiki/") > text.find("<td><a href=\"/w/index.php?title=")):
+            text = text[text.find("(page does not exist)")+10:]
+            count += 1
+        else:
+            start = text.find("<td><a href=\"/wiki/")
+            text = text[start+1:]
+            end = text.find("title")
+            if(count %2 == 0):
+                company_name = text[18:end-2]
+                print(company_name)
+                link = "https://en.wikipedia.org/wiki/" + company_name
+                #company_text[company_name] = file_cleaner(link)
+                writer.writerow([company_name, file_cleaner(link)])
+            count += 1
+            text = text[end:]
