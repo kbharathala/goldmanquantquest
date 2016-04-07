2851acaef8ee5aae7bee49dbdc1ea2ad5441566d
kbharathala <kbharathala@gmail.com>
2016-04-06 21:27:49 -0400

    Modified

--- wikiScraper.py
+++ wikiScraper.py
@@ -2,10 +2,17 @@ import urllib2
 import re
 import csv
 
+from nltk.tokenize import RegexpTokenizer
+from stop_words import get_stop_words
+from nltk.stem.porter import PorterStemmer
+
 BASE_URL = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 COMPANY_COUNT = 1008
 
 def file_cleaner(link):
+    tokenizer = RegexpTokenizer(r'\w+')
+    p_stemmer = PorterStemmer()
+
     f = urllib2.urlopen(link)
     text = f.read()
 
@@ -24,12 +31,21 @@ def file_cleaner(link):
 
     # Removes everything between brackets.
     text = re.sub(r'\[[^)]*\]', '', text)
-    return text
+
+    # Tokenizing and removing common stop words
+    tokens = tokenizer.tokenize(text.lower())
+    tokens = [unicode(i, 'ascii', 'ignore') for i in tokens]
+    tokens = [str(i) for i in tokens if not i in get_stop_words('en')]
+    tokens = [i for i in tokens if len(i) > 2]
+
+    return tokens
+
+#print(file_cleaner("https://en.wikipedia.org/wiki/3M"))
 
 f = urllib2.urlopen(BASE_URL)
 text = f.read()
 
-with open('results.csv', 'wb') as f:
+with open('resultsTok.csv', 'wb') as f:
     writer = csv.writer(f)
     count = 0
     company_links = []
@@ -46,8 +62,9 @@ with open('results.csv', 'wb') as f:
                 company_name = text[18:end-2]
                 print(company_name)
                 link = "https://en.wikipedia.org/wiki/" + company_name
+                clean_file = file_cleaner(link)
                 #company_text[company_name] = file_cleaner(link)
-                writer.writerow([company_name, file_cleaner(link)])
+                writer.writerow([company_name] + clean_file)
                 if(company_name == "Zoetis"):
                     break
             count += 1
