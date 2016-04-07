404e0ecc02bc72cc5003c8f6aacb291149d738fe
krikumar <krikumar@sas.upenn.edu>
2016-04-06 22:14:00 -0400

    Update lda.py

--- lda.py
+++ lda.py
@@ -1,21 +1,14 @@
-#import genism 
+#import genism and the dictionary docDict
 from gensim import corpora, models
-import csv
-
-#create a dictionary with the texts
-dictionary = corpora.Dictionary(excelToDictionary.docDict)
-
-#this turns corpus into a list of vectors equal to the nunmber of docs
-#in each doc vector there are tuples corresponding to (term ID, term freq)
-corpus = [dictionary.doc2bow(excelToDictionary.docDict) for text in excelToDictionary.docDict]
+from excelToDictionary import docDict 
 
 #now we can create the lda model
 #n is the number of topics that we want
 #p is the number of laps the model takes through corpus
-ldamodel = genism.models.ldamodel.LdaModel(corpus, num_topics=5, id2word = dictionary, passes=1)
+ldamodel = genism.models.ldamodel.LdaModel(docDict, num_topics=5, id2word = dictionary, passes=1)
 
 #printing will give us the freq of a word in a doc a
-corpus_lda = lda[corpus]
+docDict_lda = lda[docDict]
 
 for l,t in izip(corpus_lda,corpus):
   print l,"#",t
