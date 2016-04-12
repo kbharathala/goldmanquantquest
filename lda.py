4074e8e6ce8229bf1f49a5dec59f70d435338263
krikumar <krikumar@sas.upenn.edu>
2016-04-11 22:04:37 -0400

    Update lda.py

--- lda.py
+++ lda.py
@@ -1,6 +1,8 @@
 import csv
 import gensim
+from bow import BagOfWords
 from gensim import corpora, models
+import pandas as pd
 
 docDict = {}
 docList = []
@@ -10,16 +12,22 @@ with open('resultsTok.csv', 'rt') as csvfile:
 	for row in reader:
 		docDict[row[0]] = row[1:]
 		docList.append(row[1:])
-	corpus = [docDict.doc2bow(csvfile) for Tok in resultsTok.csv]
 
 #import genism 
 
 #this turns corpus into a list of vectors equal to the nunmber of docs
 #in each doc vector there are tuples corresponding to (term ID, term freq)
+print('here1')
+texts = pd.read_csv('resultsTok.csv', header=0, delimiter="/t", quoting=3 )
+texts_data_features = vectorizer.transform(texts)
+print texts.shape
+
+#Create am empty list and append the clean reviews one by one 
+print('here2')
 #now we can create the lda model
 #n is the number of topics that we want
 #p is the number of laps the model takes through corpus
-ldamodel = genism.models.ldamodel.LdaModel(corpus, num_topics=5, id2word = docDict, passes=1)
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=5, id2word = docDict, passes=1)
 
 #printing will give us the freq of a word in a doc a
 corpus_lda = lda[corpus]
