b37496a7a9d1881c52bcfc095c2f8c2e4407244c
natashanarang <nnarang777@gmail.com>
2016-04-12 20:19:07 -0400

    Updated locations of companies

--- wikiScraper.py
+++ wikiScraper.py
@@ -1,6 +1,7 @@
 import urllib2
 import re
 import csv
+import math
 
 from nltk.tokenize import RegexpTokenizer
 from stop_words import get_stop_words
@@ -9,6 +10,52 @@ from nltk.stem.porter import PorterStemmer
 BASE_URL = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 COMPANY_COUNT = 1008
 company_equities = {}
+company_industries = {}
+company_locations = {}
+all_states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", 
+"California", "Colorado", "Connecticut", "District ", "of Columbia", 
+"Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", 
+"Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", 
+"Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", 
+"Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", 
+"New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", 
+"Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", 
+"South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", 
+"Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+
+def company_industry_multiples(company1, company2):
+    if (company1 not in company_industries):
+        print("Error: company not in dictionary")
+    elif (company2 not in company_industries):
+        print("Error: company not in dictionary")
+    else:
+        company1_info = company_industries[company1]
+        company2_info = company_industries[company2]
+
+        if (company1_info[1] == company2_info[1]):
+            toMultiply = math.sqrt(4)
+        elif (company1_info[0] == company2_info[0]):
+            toMultiply = math.sqrt(2)
+        else:
+            toMultiply = math.sqrt(1)
+        print(toMultiply)
+        return toMultiply
+
+def company_location_multiples(company1, company2):
+    if (company1 not in company_locations):
+        print("Error: company not in dictionary")
+    elif (company2 not in company_locations):
+        print("Error: company not in dictionary")
+    else:
+        company1_location = company_locations[company1]
+        company2_location = company_locations[company2]
+
+        if (company1_location == company2_location):
+            toMultiply = math.sqrt(1.5)
+        else:
+            toMultiply = 1
+        print(toMultiply)
+        return toMultiply
 
 def file_cleaner(link, company_name):
     tokenizer = RegexpTokenizer(r'\w+')
@@ -56,7 +103,6 @@ def file_cleaner(link, company_name):
 
 
 
-
     # Tokenizing and removing common stop words
     tokens = tokenizer.tokenize(text.lower())
     tokens = [unicode(i, 'ascii', 'ignore') for i in tokens]
@@ -74,7 +120,6 @@ with open('resultsTok.csv', 'wb') as f:
     writer = csv.writer(f)
     count = 0
     company_links = []
-    company_industries = {}
     #company_text = {}
     while((text.find("<td><a href=\"/wiki/") != -1 or text.find("<td><a href=\"/w/index.php?title=") != -1) and count < COMPANY_COUNT*2 + 8):
         if(text.find("<td><a href=\"/wiki/") > text.find("<td><a href=\"/w/index.php?title=")):
@@ -88,18 +133,45 @@ with open('resultsTok.csv', 'wb') as f:
             industrial_text = text[start_industrial+1:]
             end_industrial = industrial_text.find("<td><a href=") - 7
             end_general_industrial = industrial_text.find("</td>");
-            if(count %2 == 0):
+            start_location = text.find("<td><a href=") 
+            location_text = text[start_location:]
+            place_start_location = location_text.find('title="') + 7
+            place_end_location = location_text.find('">')
+            end_location = text.find("</a></td>");
+    
+            if(count % 2 == 0):
+                company_name = text[18:end-2]
                 general_industry_text = industrial_text[:end_general_industrial]
                 specific_industry_text = industrial_text[end_general_industrial + 10 : end_industrial +1]
-                company_name = text[18:end-2]
-                #print(company_name)
+                place_text = location_text[place_start_location:place_end_location]
+                place_contains_class = place_text.find(" class=")
+                if (place_contains_class > 0):
+                    place_text = place_text[:place_contains_class - 1]
+                if (place_text.find(",") > 0):
+                    place_text = place_text.split(",")
+                    if (place_text[1].strip() in all_states):
+                        company_locations[company_name] = "United States"
+                    else:
+                        company_locations[company_name] = place_text[1]
+                else:
+                    company_locations[company_name] = place_text              
+
                 company_industries[company_name] = (general_industry_text, specific_industry_text)
                 link = "https://en.wikipedia.org/wiki/" + company_name
                 clean_file = file_cleaner(link, company_name)
-                #company_text[company_name] = file_cleaner(link)
                 writer.writerow([company_name] + clean_file)
                 if(company_name == "Zoetis"):
                     break
             count += 1
             text = text[end:]
     print(company_industries)
+    print(company_locations)
+    #Testing both functions
+    #company_industry_multiples("3M", "AbbVie")
+    #company_industry_multiples("AFLAC_Inc", "Affiliated_Managers_Group_Inc")
+    #company_industry_multiples("Interpublic_Group", "Omnicom_Group")
+    #company_location_multiples("3M", "AbbVie")
+    #company_location_multiples("AFLAC_Inc", "Affiliated_Managers_Group_Inc")
+    #company_location_multiples("Interpublic_Group", "Omnicom_Group") 
+    #company_location_multiples("Extra_Space_Storage", "Transocean")   
+    #company_location_multiples("3M", "Transocean") 
